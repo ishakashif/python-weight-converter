bd26d9f7b40d5db4da4c861fd43db2fd3e400ba1
Isha Kashif <ishak@udel.edu>
2025-08-04 08:22:01 -0400

    fixed print statements

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ unit = input("Kilograms or Pounds? (K or L): ")
 
 if unit == "K":
     weight = weight * 2.205
+    print(f"Your weight is: {weight} {unit}")
     unit = "Lbs."
 elif unit == "L":
     weight = weight / 2.205
     unit = "Kgs."
-else: print(f"{unit} was not valid")
+    print(f"Your weight is: {weight} {unit}")
+else:
+    print(f"{unit} was not valid")
 
-print(f"Your weight is: {weight} {unit}")
+
