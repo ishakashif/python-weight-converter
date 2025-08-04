1c42184c2d00a4a3a7276cb8ff4d46eeda4824ed
Isha Kashif <ishak@udel.edu>
2025-08-04 08:24:41 -0400

    rounding and print statement changes

--- main.py
+++ main.py
@@ -3,15 +3,14 @@
 weight = float(input("Enter your weight: "))
 unit = input("Kilograms or Pounds? (K or L): ")
 
-if unit == "K":
+if unit == "K" or unit == "k":
     weight = weight * 2.205
-    print(f"Your weight is: {weight} {unit}")
+    print(f"Your weight is: {round(weight, 1)} {unit}")
     unit = "Lbs."
-elif unit == "L":
+elif unit == "L" or unit == "l":
     weight = weight / 2.205
     unit = "Kgs."
-    print(f"Your weight is: {weight} {unit}")
+    print(f"Your weight is: {round(weight, 1)} {unit}")
 else:
-    print(f"{unit} was not valid")
-
+    print(f"{unit} is not a valid unit")
 
